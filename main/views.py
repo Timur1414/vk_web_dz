2db3d3f81ae86956d4a339a07814896e10e55534
Timur <tima.temirov.2005@mail.ru>
2025-10-05 23:01:43 +0300

    Added base and index templates.

--- main/views.py
+++ main/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index_page(request):
+    context = {
+        'title': 'AskPupkin',
+    }
+    return render(request, 'index/index.html', context)
+
+
+def question_page(request):
+    context = {
+        'title': 'Question',
+    }
+    return render(request, 'question/question.html', context)
+
+
+def ask_page(request):
+    context = {
+        'title': 'Ask',
+    }
+    return render(request, 'question/ask.html', context)
--- vk_dz/urls.py
+++ vk_dz/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 # from django.contrib import admin
 from django.urls import path
+from main import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     #    path('admin/', admin.site.urls),
+    path('', views.index_page, name='index'),
+    path('question/', views.question_page, name='question'),
+    path('ask/', views.ask_page, name='ask'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
 ]
