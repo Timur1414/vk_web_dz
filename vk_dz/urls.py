5b3cbde6437f468827432089fd4076670723eebc
Timur <tima.temirov.2005@mail.ru>
2025-10-08 11:16:37 +0300

    Added registration template.

--- vk_dz/urls.py
+++ vk_dz/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 # from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from main import views
 from django.contrib.auth import views as auth_views
 
@@ -28,4 +28,5 @@ urlpatterns = [
     path('tag/', views.tag_page, name='tag'),
     path('settings/', views.settings_page, name='settings'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('registration/', include('django_registration.backends.one_step.urls')),
 ]
