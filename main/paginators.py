bc197d99340f943ea9cb2641a90becebc86db59e
Timur <tima.temirov.2005@mail.ru>
2025-10-08 14:48:49 +0300

    Added helpful functions for paginator.

--- main/paginators.py
+++ main/paginators.py
@@ -1,16 +1,16 @@
 # from django.views.generic import ListView
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage
 
 
-def paginate(objects_list, request, per_page=10):
+def paginate(objects_list, request, per_page=10) -> list:
     paginator = Paginator(objects_list, per_page)
     page_number = request.GET.get('page')
     try:
         page_number = int(page_number)
-    except ValueError:
-        return
-    page_obj = paginator.get_page(page_number)
-    return page_obj.object_list
+        page_obj = paginator.get_page(page_number)
+        return page_obj.object_list
+    except (EmptyPage, ValueError):
+        return []
 
 # class QuestionListView(ListView):
 #     paginate_by = 2
--- main/views.py
+++ main/views.py
@@ -1,49 +1,60 @@
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth import logout
 from django.shortcuts import redirect
+from django.template.loader import render_to_string
 
 
-def index_page(request):
+def render_questions(questions: list, request: WSGIRequest) -> str:
+    if not questions:
+        return '<p>nothing...</p>'
+    html = ''
+    for question in questions:
+        html += render_to_string(request=request, template_name='question/card.html', context={'question': question})
+    return html
+
+def index_page(request: WSGIRequest) -> HttpResponse:
     context = {
         'title': 'AskPupkin',
     }
     return render(request, 'index/index.html', context)
 
 
-def logout_view(request):
+def logout_view(request: WSGIRequest) -> HttpResponseRedirect:
     logout(request)
     return redirect('index')
 
 
-def hot_questions_page(request):
+def hot_questions_page(request: WSGIRequest) -> HttpResponse:
     context = {
         'title': 'Hot',
     }
     return render(request, 'question/hot_questions.html', context)
 
 
-def question_page(request):
+def question_page(request: WSGIRequest) -> HttpResponse:
     context = {
         'title': 'Question',
     }
     return render(request, 'question/question.html', context)
 
 
-def ask_page(request):
+def ask_page(request: WSGIRequest) -> HttpResponse:
     context = {
         'title': 'Ask',
     }
     return render(request, 'question/ask.html', context)
 
 
-def tag_page(request):
+def tag_page(request: WSGIRequest) -> HttpResponse:
     context = {
         'title': 'Tag',
     }
     return render(request, 'tag/index.html', context)
 
 
-def settings_page(request):
+def settings_page(request: WSGIRequest) -> HttpResponse:
     context = {
         'title': 'Settings',
     }
